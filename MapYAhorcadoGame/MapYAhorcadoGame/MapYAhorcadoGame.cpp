8dc81e2fbf3c751122fca714bbd9edacb8609649
lmorenope8 <lauramoreno@vedrunavall.cat>
2024-10-23 19:33:26 +0200

    test

--- MapYAhorcadoGame/MapYAhorcadoGame/MapYAhorcadoGame.cpp
+++ MapYAhorcadoGame/MapYAhorcadoGame/MapYAhorcadoGame.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+	char map[10][10], usuario='X', joc='R';
+	int movimiento;
+	for (int i = 0; i < 10; i++) {
+		for(int j=0; j< 10; j++){
+			map[i][j] = '-';
+		}
+	}
+	map[4][4] = joc;
+	map[0][0] = usuario;
 
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%c", map[i][j]);
+		}
+		printf("\n");
+	}
 }
